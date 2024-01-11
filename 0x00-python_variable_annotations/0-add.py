fd934d40b661d15483b0d11fd4c39238592a27dd
ygyuri <gideonyuri15@gmail.com>
2024-01-11 04:39:07 -0800

    add

--- 0x00-python_variable_annotations/0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-""" FUNCTION FOR SUM USING BASIC ANNOTATIONS """
-
-
-def add(a: float, b: float) -> float:
-    """ Returns sum of a + b """
-    return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-""" FUNCTION TO CONCAT A STRINGS """
-
-
-def concat(str1: str, str2: str) -> str:
-    """ Returns a concatenated string """
-    return str1 + str2
